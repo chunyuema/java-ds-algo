0726d7942cbfce0110142068b72aa2e9a5097d07
Chunyue Ma <cma01@wesleyan.edu>
2021-09-03 20:14:08 -0400

    binary search tree implementation #5

---
+++ src/com/chunyue/tree/BSTDemoMain.java
@@ -0,0 +1,2 @@
+package com.chunyue.tree;public class BSTDemoMain {
+}
---
+++ src/com/chunyue/tree/BinarySearchTree.java
@@ -0,0 +1,16 @@
+package com.chunyue.tree;
+
+public class BinaryTreeSort<E extends Integer> {
+    private Node root;
+
+    public void add(E element){
+        Node<E> node = new Node<>(element);
+        if (this.root == null) this.root = node;
+        else this.root.addNode(node);
+    }
+
+    public void sort(){
+        if (this.root == null) return;
+        this.root.inOrderTraversal();
+    }
+}
