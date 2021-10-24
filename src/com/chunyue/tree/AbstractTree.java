be489e1da1f57989782ddc977c06042f7e33cd86
Chunyue Ma <cma01@wesleyan.edu>
2021-10-23 20:30:30 -0400

    add comments for abstract tree class as the base class for tree implementation #5

--- src/com/chunyue/tree/AbstractTree.java
+++ src/com/chunyue/tree/AbstractTree.java
@@ -2,24 +2,38 @@ package com.chunyue.tree;
 
 import com.chunyue.Position;
 
+// provide some concrete implementations for certain methods; serve as the base class for more concrete
+// implementation of trees
 public abstract class AbstractTree<E> implements Tree<E> {
+
     public boolean isInternal(Position<E> p) {return numChildren(p) > 0;}
     public boolean isExternal(Position<E> p) {return numChildren(p) == 0;}
     public boolean isRoot(Position<E> p) {return p == root();}
     public boolean isEmpty() {return size() == 0;}
 
-    // return the level of nodes from p to its root of the tree
+    // return the level of nodes from p to its root of the tree -> number of ancestors of p other than p itself
+    // time complexity: O(n), n = number of positions in the tree
     public int depth(Position<E> p){
         if (isRoot(p)) return 0;
         else return 1 + depth(parent(p));
     }
 
-    // return the height of the subtree that is rooted at the position p
-    public int height(Position<E> p){
-        int height = 0;
-        for (Position<E> c : children(p)){
-            height = Math.max(height, 1 + height(c));
+    // return the height of the subtree that is rooted at the position p -> equal to the max depths of its positions
+    // time complexity: O(n^2), bad implementation
+    public int heightBad(Position<E> p){
+        int h = 0;
+        for (Position<E> c : positions()){
+            h = Math.max(h, depth(c));
         }
-        return height;
+        return h;
+    }
+
+    // time complexity: O(n^2), good implementation
+    public int height(Position<E> p){
+        int h = 0;
+        for (Position<E> c: children(p)){
+            h = Math.max(h, height(c));
+        }
+        return h;
     }
 }
