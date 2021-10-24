25cad91b2e94a1e44723c5d9a1291762f9f14d6e
Chunyue Ma <cma01@wesleyan.edu>
2021-10-23 20:35:40 -0400

    add comments for binary tree data type (binary tree interface) #5

--- src/com/chunyue/tree/BinaryTree.java
+++ src/com/chunyue/tree/BinaryTree.java
@@ -1,10 +1,14 @@
 package com.chunyue.tree;
 
 import com.chunyue.Position;
-import javafx.geometry.Pos;
 
 public interface BinaryTree<E> extends Tree<E>{
+    // return the position of the left child of p
     Position<E> left(Position<E> p) throws IllegalArgumentException;
+
+    // return the position of the right child of p
     Position<E> right(Position<E> p) throws IllegalArgumentException;
+
+    // return the position of the sibling of p
     Position<E> sibling(Position<E> p) throws IllegalArgumentException;
 }
